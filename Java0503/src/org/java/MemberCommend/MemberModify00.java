91def0ba0185567908e869e99a70016c1eb8de87
rlaqjavlf <rlaqjavlf123@naver.com>
2019-05-07 09:04:15 +0900

    v2

---
+++ Java0503/src/org/java/MemberCommend/MemberModify00.java
@@ -0,0 +1,16 @@
+package org.java.MemberCommend;
+
+public class MemberModify00 implements QueryCommend{
+
+	@Override
+	public void excuteCommend() {
+		// TODO Auto-generated method stub
+			System.out.println("회원 수정 ");	
+	
+			//userID, userPW,userName, userAge
+			//Scanner를 이용해서 입력하고
+			//MemberDAO에 Update메서드 구현 
+			
+	}
+	
+}
--- Java0503/src/org/java/MemberCommend/UpdateDo.java
+++ Java0503/src/org/java/MemberCommend/UpdateDo.java
@@ -1,10 +1,25 @@
 package org.java.MemberCommend;
 
+import java.util.Scanner;
+
+import org.java.MemberDAO.MemberDAO;
+import org.java.MemberDTO.MemberDTO;
+
 public class UpdateDo implements QueryCommend {
 
 	@Override
 	public void excuteCommend() {
 		// TODO Auto-generated method stub
+		System.out.println("회원 수정 ");
+		
+		MemberDAO DAO =new MemberDAO();
+		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("아이디를 입력하세요 : ");
+		
+		String userId=sc.next();
+
 		
 	}
 
